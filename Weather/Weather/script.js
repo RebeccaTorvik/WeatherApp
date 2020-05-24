40447a078a5feb923c5808989329975648bacba2
RebeccaTorvik <37801832+RebeccaTorvik@users.noreply.github.com>
2020-05-23 23:13:40 -0400

    Add files via upload

---
+++ Weather/Weather/script.js
@@ -0,0 +1,93 @@
+let appId = '851c541b3f67fffaafa84038d6dedf6c';
+let units = 'imperial';
+let searchMethod;
+
+function getSearchMethod(searchTerm) {
+    if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+        searchMethod = 'zip';
+    else 
+        searchMethod = 'q';
+}
+
+function searchWeather(searchTerm) {
+    getSearchMethod(searchTerm);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+        return result.json();
+    }).then(result => {
+        init(result);
+    })
+}
+
+function init(resultFromServer) {
+   switch (resultFromServer.weather[0].main) {
+       case 'Clear':
+           document.body.style.backgroundImage = 'url("Images/clear.jpg")';
+           break;
+
+        case 'Clouds':
+            document.body.style.backgroundImage = 'url("Images/cloudy.jpg")';
+            break;
+
+        case 'Rain':
+        case 'Drizzle':
+        case 'Mist':
+            document.body.style.backgroundImage = 'url("Images/rain.jpg")';
+            break;
+
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = 'url("Images/storm.jpg")';
+            break;
+
+        case 'Snow':
+            document.body.style.backgroundImage = 'url("Images/snow.jpg")';
+            break;
+
+       default:
+           break;
+   }
+
+   let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+   let temperatureElement = document.getElementById('temperature');
+   let humidityElement = document.getElementById('humidity');
+   let windSpeedElement = document.getElementById('windSpeed');
+   let cityHeader = document.getElementById('cityHeader');
+   let weatherIcon = document.getElementById('documentIconImg');
+   let temp_minElement = document.getElementById('temp_min');
+   let temp_maxElement = document.getElementById('temp_max');
+
+   weatherIcon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.weather[0].icon + '.png';
+
+   let resultDescription = resultFromServer.weather[0].description;
+   weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+
+   temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176';
+   temp_minElement.innerHTML = Math.floor(resultFromServer.main.temp_min) + '&#176';
+   temp_maxElement.innerHTML = Math.floor(resultFromServer.main.temp_max) + '&#176';
+   windSpeedElement.innerHTML = 'Winds at ' + Math.floor(resultFromServer.wind.speed) + ' mph';
+   cityHeader.innerHTML = resultFromServer.name;
+   humidityElement.innerHTML = 'Humidity levels at ' + resultFromServer.main.humidity + '%';
+
+   setPositionForWeatherInfo();
+}
+
+function setPositionForWeatherInfo() {
+    let weatherContainer = document.getElementById('weatherContainer');
+    let weatherContainerHeight = weatherContainer.clientHeight;
+    let weatherContainerWidth = weatherContainer.clientWidth;
+
+    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
+    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3}px)`;
+    weatherContainer.style.visibility = 'visible';
+}
+
+
+
+
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
+    if(searchTerm)
+        searchWeather(searchTerm);
+})
+
+
